c5fa2bdaa03340c1e054190720a706365930de2b
Mason Chang <mchang@mozilla.com>
2014-06-08 21:02:23 -0700

    update reqs when we run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+echo "Running updating b2gperf reqs. This will ask for sudo. Please provide the sudo password."
+sudo ./installReqs.sh
+if [ $? -ne 0 ]
+then
+  echo "Could not update b2g perf requirements. Please run installReqs.sh manually and see the error"
+  exit 1
+fi
 
 # Runs forever!
 while true
